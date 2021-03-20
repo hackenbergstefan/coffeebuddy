233cf4cf2cc5ecd17e45f5d42df351fd4d999537
Johannes Holland <joh.ho@gmx.de>
2021-03-20 18:28:09 +0100

    Add first coffee stats

--- coffeetag/model.py
+++ coffeetag/model.py
@@ -1,4 +1,5 @@
 import datetime
+from sqlalchemy import text
 
 from coffeetag import db
 
@@ -21,6 +22,38 @@ class User(db.Model):
         # TODO: Fast enough?
         return sum(c.price for c in self.coffees) - sum(p.amount for p in self.pays)
 
+    def drinks(self, date=None):
+        drinks = Drink.query.filter(self.id == Drink.userid)
+        if date is not None:
+            drinks = drinks.filter(db.func.Date(Drink.timestamp) == date)
+        drinks = drinks.order_by(Drink.timestamp)
+        return drinks
+
+    def nth_drink(self, date, n):
+        return self.drinks(date).limit(n)[-1]
+
+    @property
+    def drinks_per_day(self):
+        return (
+            db.session.query(db.func.Date(Drink.timestamp), db.func.count(db.func.Date(Drink.timestamp)))
+            .filter(self.id == Drink.userid)
+            .group_by(db.func.Date(Drink.timestamp))
+        )
+
+    @property
+    def max_drinks_per_day(self):
+        _date, drinks = self.drinks_per_day.order_by(text("count_1"))[-1]
+        return drinks
+
+    @property
+    def drink_days(self):
+        return (
+            tup[0] for tup in
+            db.session.query(db.func.Date(Drink.timestamp))
+            .filter(self.id == Drink.userid)
+            .distinct()
+        )
+
     def __repr__(self):
         return f'<User tag={self.tag} name={self.name} prename={self.prename}>'
 
@@ -36,6 +69,9 @@ class Drink(db.Model):
         if 'timestamp' not in kwargs:
             kwargs['timestamp'] = datetime.datetime.now()
         super().__init__(*args, **kwargs)
+
+    def by_date(date):
+        return Drink.query.filter(db.func.Date(Drink.timestamp) == date)
 
 
 class Pay(db.Model):
