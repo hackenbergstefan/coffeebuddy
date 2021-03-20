468fd1d26f15b08ac116eace715ef1d9a91bdd6c
Stefan Hackenberg <mail@stefan-hackenberg.de>
2021-03-20 22:19:21 +0100

    Restructure tests

--- test/__init__.py
+++ test/__init__.py
@@ -2,6 +2,8 @@ import unittest
 import flask
 import coffeetag
 
+from coffeetag.model import User
+
 
 class TestCoffeetag(unittest.TestCase):
     def setUp(self):
@@ -19,3 +21,11 @@ class TestCoffeetag(unittest.TestCase):
         for table in reversed(self.db.metadata.sorted_tables):
             self.db.session.execute(table.delete())
         self.db.session.commit()
+
+    def add_default_user(self):
+        user1 = User(tag=b'\x01\x02\x03', name='Mustermann', prename='Max')
+        self.db.session.add(user1)
+        user2 = User(tag=b'\x04\x05\x06', name='Doe', prename='Jane')
+        self.db.session.add(user2)
+        self.db.session.commit()
+        return user1, user2
--- test/test_app.py
+++ test/test_app.py
@@ -6,17 +6,7 @@ from coffeetag.model import Drink, User, Pay
 from . import TestCoffeetag
 
 
-class TestUsers(TestCoffeetag):
-    def test_non_existing_user(self):
-        response = self.client.get('/coffee.html?tag=00', follow_redirects=True)
-        self.assertIn(b'Card not found', response.data)
-
-    def test_existing_user(self):
-        self.db.session.add(User(tag=b'\x01\x02\x03', name='Mustermann', prename='Max'))
-        self.db.session.commit()
-        response = self.client.get('/coffee.html?tag=010203')
-        self.assertEqual(response.status_code, 200)
-
+class TestDatabase(TestCoffeetag):
     def test_drink_coffee(self):
         user1 = User(tag=b'\x01\x02\x03', name='Mustermann', prename='Max')
         user2 = User(tag=b'\x03\x04\x05', name='Doe', prename='Jane')
@@ -59,7 +49,9 @@ class TestUsers(TestCoffeetag):
 
         self.assertEqual(user.unpayed, 30)
 
-    def test_edituser(self):
+
+class TestRouteEdituser(TestCoffeetag):
+    def test_adduser(self):
         response = self.client.post('/edituser.html', data=dict(
             tag='01 02 03 04',
             last_name='Mustermann',
@@ -72,17 +64,39 @@ class TestUsers(TestCoffeetag):
         self.assertEqual(users[0].name, 'Mustermann')
         self.assertEqual(users[0].prename, 'Max')
 
-    def test_add_existing_user_fails(self):
-        response = self.client.post('/edituser.html', data=dict(
+    def test_edituser(self):
+        self.db.session.add(User(tag=b'\x01\x02\x03\x04', name='Mustermann', prename='Max'))
+        response = self.client.post(f'/edituser.html?tag=01020304', data=dict(
             tag='01 02 03 04',
-            last_name='Mustermann',
-            first_name='Max',
+            last_name='Doe',
+            first_name='Jane',
         ))
         self.assertEqual(response.status_code, 302)
-        with self.assertRaises(sqlalchemy.exc.IntegrityError):
-            self.client.post('/edituser.html', data=dict(
-                tag='01 02 03 04',
-                last_name='Mustermann',
-                first_name='Max',
-            ))
-        self.db.session.rollback()
+        users = User.query.all()
+        self.assertEqual(len(users), 1)
+        self.assertEqual(users[0].tag, b'\x01\x02\x03\x04')
+        self.assertEqual(users[0].name, 'Doe')
+        self.assertEqual(users[0].prename, 'Jane')
+
+
+class TestRouteCoffee(TestCoffeetag):
+    def test_non_existing_user(self):
+        response = self.client.get('/coffee.html?tag=00', follow_redirects=True)
+        self.assertIn(b'Card not found', response.data)
+
+    def test_existing_user(self):
+        self.add_default_user()
+        response = self.client.get('/coffee.html?tag=010203')
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        self.add_default_user()
+        response = self.client.post('/coffee.html?tag=010203', data=dict(logout=''))
+        self.assertEqual(response.status_code, 302)
+
+    def test_undopay(self):
+        user, _ = self.add_default_user()
+        user.pays.append(Pay(amount=10))
+        response = self.client.post('/coffee.html?tag=010203', data=dict(undopay=''))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(user.pays), 0)
