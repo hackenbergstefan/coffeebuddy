15214722b0027da56f2d1f37909c3e6849c1eaf4
Johannes Holland <joh.ho@gmx.de>
2021-07-11 15:50:52 +0200

    Re-add sqlite and move config to config.py

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
+import socket
+
  # Price per cup in €
 PRICE = 0.30
  # Amount in € to pay by one pay
 PAY = 10
 # Card type
 CARD = 'PCSC'
+# Database connection details
+DB_BACKEND = 'postgres'
+DB_HOST = 'coffeebuddydb'
+DB_PORT = 5432
+DB_USER = socket.gethostname()
