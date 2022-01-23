9c6e8dc712dbd5e6233cbdaee0b1eca9b1a2ed9f
Stefan Hackenberg <mail@stefan-hackenberg.de>
2022-01-23 07:51:41 +0100

    Add more information on welcome

--- coffeebuddy/route_welcome.py
+++ coffeebuddy/route_welcome.py
@@ -1,4 +1,6 @@
 import datetime
+import socket
+import subprocess
 
 import flask
 
@@ -8,5 +10,12 @@ from coffeebuddy.model import Drink
 def init():
     @flask.current_app.route('/')
     def welcome():
-        data = [(amount, date.strftime('%Y-%m-%d')) for amount, date in Drink.drinks_vs_days(datetime.timedelta(weeks=12))]
-        return flask.render_template('welcome.html', dataset=data)
+        data = [
+            (amount, date.strftime('%Y-%m-%d')) for amount, date in Drink.drinks_vs_days(datetime.timedelta(weeks=12))
+        ]
+        return flask.render_template(
+            'welcome.html',
+            dataset=data,
+            hostname=socket.gethostname(),
+            githash=subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip(),
+        )
