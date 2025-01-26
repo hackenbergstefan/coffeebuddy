42e126cad1e166c042b1713251dc4890c1be8c67
Stefan Hackenberg <mail@stefan-hackenberg.de>
2025-01-26 14:23:38 +0100

    Commit forgotten file

---
+++ coffeebuddy/extensions/help.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import qrcode
+from qrcode.image.svg import SvgPathImage
+
+from coffeebuddy import __version__
+
+"""
+This extension just generates a qr code to the help pages.
+"""
+
+HELP_URL = f"github.com/hackenbergstefan/coffeebuddy/blob/v{__version__}/HELP.md"
+
+
+def init():
+    qr = qrcode.QRCode(border=0, box_size=10, image_factory=SvgPathImage)
+    qr.add_data(HELP_URL)
+    qr.make(fit=True)
+    (Path(__file__).parent.parent / "static" / "qrhelp.svg").write_bytes(
+        qr.make_image(attrib={"class": "qrhelp"}).to_string()
+    )
