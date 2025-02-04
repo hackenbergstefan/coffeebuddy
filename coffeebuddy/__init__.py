176234286d9dfed3b1d744f3b98d8f60164c82eb
Stefan Hackenberg <mail@stefan-hackenberg.de>
2025-02-04 15:25:08 +0100

    Fix display of price at favorites and update jura_ble

--- coffeebuddy/__init__.py
+++ coffeebuddy/__init__.py
@@ -8,7 +8,7 @@ from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
-__version__ = "2.0.4"
+__version__ = "2.0.5"
 
 
 class Base(DeclarativeBase):
